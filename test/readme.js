69a62714bbe0d4168abbd44343026d9ad6968c9e
rjrodger <richard@ricebridge.com>
2019-05-06 00:03:20 +0100

    readme

--- test/readme.js
+++ test/readme.js
@@ -1,23 +1,8 @@
-# seneca-owner
-[Seneca](senecajs.org) plugin providing ownership permissions for entities.
-
-[![Npm][BadgeNpm]][Npm]
-[![Travis][BadgeTravis]][Travis]
-[![Coveralls][BadgeCoveralls]][Coveralls]
-
-This plugin blocks access to entities unless certain fields match
-custom values in the Seneca instance. It also ensures that entities
-are saved with approporiate field values.
-
-
-## Quick Example
-
-```
 require('seneca')
   .test()
   .use('promisify')
   .use('entity')
-  .use('owner', {
+  .use('..', {
     fields: ['usr','org'],
     annotate: [
       'role:entity,cmd:save,base:zed',
@@ -67,18 +52,3 @@ require('seneca')
     console.log(not_a2) // null
     console.log(not_a1) // null
   })
-```
-
-## Usage
-
-For an example showing groups with custom permissions, see the
-`group-scenario` unit test.
-
-
-
-[BadgeCoveralls]: https://coveralls.io/repos/voxgig/seneca-owner/badge.svg?branch=master&service=github
-[BadgeNpm]: https://badge.fury.io/js/seneca-owner.svg
-[BadgeTravis]: https://travis-ci.org/voxgig/seneca-owner.svg?branch=master
-[Coveralls]: https://coveralls.io/github/voxgig/seneca-owner?branch=master
-[Npm]: https://www.npmjs.com/package/seneca-owner
-[Travis]: https://travis-ci.org/voxgig/seneca-owner?branch=master
